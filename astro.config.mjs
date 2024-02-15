d27d1b807be7238217e0cb5f1c509de4209f0205
Dave Hart <iamdavehart@gmail.com>
2024-02-15 15:28:27 +0000

    domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://missionbeyond.github.io",
-  base: "/labs",
+  site: "https://labs.missionbeyond.co.uk",
   integrations: [react(), tailwind(), icon()]
 });
